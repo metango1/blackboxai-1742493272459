f7c4cdb213eabde547202c6ec477d529e49a44e4
metango1 <browser.tarar+1@gmail.com>
2025-03-20 22:54:52 +0500

    Add Controllers/HomeController.cs 1742493290936

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace H82Travels.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
